93f4f9f4409e37fcaf469627af25a8b5443b7442
gerry <gerry.scott99@gmail.com>
2020-10-24 15:08:39 -0400

    Code Structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
 using namespace std;
+
+struct ATerm
+{
+    int Coefficient;
+    int Exponent;
+    ATerm* Next;
+};
+typedef ATerm* Polynomial;
+
+bool Check_error(string);
+Polynomial Add_Poly(Polynomial);
+Polynomial Multiply_Poly(Polynomial);
+
 int main() {
+
     cout << "Hello, World!" << endl;
 
 }
+
+bool Check_error(string)
+{
+    bool error;
+    return error;
+}
+
+Polynomial Add_Poly(Polynomial)
+{
+    return nullptr;
+}
+
+Polynomial Multiply_Poly(Polynomial)
+{
+    return nullptr;
+}
+
