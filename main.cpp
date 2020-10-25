72d1ac9318ebdebae9647964ee7e1e90be52a5f9
gerry <gerry.scott99@gmail.com>
2020-10-25 04:39:00 -0400

    -Check_Error() function needs to implement a check for symbols +/- -Program ignores spaces in user input and stores strings into a string array without spaces.

--- main.cpp
+++ main.cpp
@@ -43,23 +43,41 @@ int main() {
     for (int i = 0; i < length;i++)
     {
         string check;
+        string no_space;
 
         cout << "Input polynomial " << i+1 << " -> ";
         getline(cin,check);
         cout << endl;
-
-        while(Check_error(check))
+//Ignore spaces and copy values into another string//
+        for (int i = 0; i < check.length();i++)
         {
+            if (!isspace(check[i]))
+            {
+                no_space += check[i];
+            }
+        }
+
+        while(Check_error(no_space))
+        {
+            no_space = "";
             cout << "Input Error: please input a correct polynomial" << endl;
             cout << "Input polynomial " << i+1 << " -> ";
             getline(cin,check);
             cout << endl;
+
+            for (int i = 0; i < check.length();i++)
+            {
+                if (!isspace(check[i]))
+                {
+                    no_space += check[i];
+                }
+            }
         }
-            polynomials[i] = check;
+            polynomials[i] = no_space;
     }
     for (int i = 0; i < length;i++)
     {
-        cout << polynomials[i] << endl;
+        cout << i << " " << polynomials[i] << endl;
     }
 
 }
@@ -72,26 +90,38 @@ bool Check_error(string polynomial)
 //Check terminal cases that do not require iterating through the string//
     if (polynomial[0] == '^' || polynomial[0] == '*' || polynomial[length - 1] == '*' || polynomial[length - 1] == '^')
     {
+        cout << "case 1";
         return !error;
     }
 //Check terminal cases that require iterating through the string//
     for (int i = 0; i < length;i++)
     {
-        if (!isdigit(polynomial[i]) && polynomial[i] != 'x' && polynomial[i] != 'X' && polynomial[i]!='^' && polynomial[i] != ' ' && polynomial[i] != '+' && polynomial[i] != '-')
+        if (!isdigit(polynomial[i]) && polynomial[i] != 'x' && polynomial[i] != 'X' && polynomial[i]!= '^' && polynomial[i] != '+' && polynomial[i] != '-')
         {
+            cout << "case 2";
             return !error;
         }
 //Check that carat is properly connected as an exponent
         else if (polynomial[i] == '^')
         {
-            if (polynomial[i -1] != 'X' && polynomial[i -1] != 'x')
+            if (polynomial[i - 1] != 'X' && polynomial[i -1] != 'x')
             {
+                cout << "case 3";
+                return !error;
+            }
+//Check for incorrect values as an exponent
+            else if (!isdigit(polynomial[i + 1]) && polynomial[ i+ 1]!= '-')
+            {
+                cout << "case 4";
+                return !error;
+            }
+//Check for negative exponents
+            else if (polynomial[i +1] == '-' && !isdigit(polynomial[i +2]))
+            {
+                cout << "Case 5";
                 return !error;
             }
 
-            else if (!isdigit(polynomial[i + 1])){
-                return !error;
-            }
         }
     }
 
